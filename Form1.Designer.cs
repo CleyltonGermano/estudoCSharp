051a29cd6800aac4b5b53e126f625abc1b80763c
CleyltonGermano <84788498+CleyltonGermano@users.noreply.github.com>
2021-08-24 14:59:32 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace JogoDaVelha2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b12 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b12
+            // 
+            this.b12.Location = new System.Drawing.Point(41, 132);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(50, 50);
+            this.b12.TabIndex = 0;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.UseWaitCursor = true;
+            // 
+            // b21
+            // 
+            this.b21.Location = new System.Drawing.Point(97, 76);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(50, 50);
+            this.b21.TabIndex = 1;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.UseWaitCursor = true;
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(41, 76);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(50, 50);
+            this.b11.TabIndex = 2;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.UseWaitCursor = true;
+            // 
+            // b31
+            // 
+            this.b31.Location = new System.Drawing.Point(153, 76);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(50, 50);
+            this.b31.TabIndex = 3;
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.UseWaitCursor = true;
+            // 
+            // b22
+            // 
+            this.b22.Location = new System.Drawing.Point(97, 132);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(50, 50);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.UseWaitCursor = true;
+            // 
+            // b32
+            // 
+            this.b32.Location = new System.Drawing.Point(153, 132);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(50, 50);
+            this.b32.TabIndex = 5;
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.UseWaitCursor = true;
+            // 
+            // b33
+            // 
+            this.b33.Location = new System.Drawing.Point(153, 188);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(50, 50);
+            this.b33.TabIndex = 6;
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.UseWaitCursor = true;
+            // 
+            // b23
+            // 
+            this.b23.Location = new System.Drawing.Point(97, 188);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(50, 50);
+            this.b23.TabIndex = 7;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.UseWaitCursor = true;
+            // 
+            // b13
+            // 
+            this.b13.Location = new System.Drawing.Point(41, 188);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(50, 50);
+            this.b13.TabIndex = 8;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 35);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X, é a sua vez!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(255, 312);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b12);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
